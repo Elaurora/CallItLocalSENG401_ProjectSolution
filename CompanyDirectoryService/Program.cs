db6c346a624105c42065a9a47db8731d46273af9
Elaurora <joshua.walters@ucalgary.ca>
2017-06-26 16:57:42 -0600

    Added CompanyDirectoryService project to solution

---
+++ CompanyDirectoryService/Program.cs
@@ -0,0 +1,45 @@
+﻿using NServiceBus;
+
+using System;
+using System.Threading.Tasks;
+
+namespace CompanyDirectoryService
+{
+    class Program
+    {
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "CompanyDirectory";
+
+            //Create a new Endpoint configuration with the name "CompanyDirectory"
+            var endpointConfiguration = new EndpointConfiguration("CompanyDirectory");
+
+            //Allows the endpoint to run installers upon startup. This includes things such as the creation of message queues.
+            endpointConfiguration.EnableInstallers();
+            //Instructs the queue to serialize messages with Json, should it need to serialize them
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            //Instructs the endpoint to use local RAM to store queues. TODO: Good during development, not during deployment (According to the NServiceBus tutorial)
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            //Instructs the endpoint to send messages it cannot process to a queue named "error"
+            endpointConfiguration.SendFailedMessagesTo("error");
+
+            //Instructs the endpoint to use Microsoft Message Queuing TOD): Consider using RabbitMQ instead, only because Arcurve reccomended it. 
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+            //This variable is used to configure how messages are routed. Using this, you may set the default reciever of a particular command, and/or subscribe to any number of events
+            var routing = transport.Routing();
+
+            //Start the endpoint with the configuration defined above. It should be noted that any changes made to the endpointConfiguration after an endpoint is instantiated will not apply to any endpoints that have already been instantiated
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+    }
+}
